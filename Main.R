7ba52ffcddbb5d52debca0f094d69fea0a49c8bd
Fernando Moyano <fmoyano@uni-goettingen.de>
2021-01-24 20:23:05 +0100

    Created main.R script.

---
+++ Main.R
@@ -0,0 +1,24 @@
+### Main
+
+## This is the main script
+## It runs all steps to carry out the simulation. This includes:
+## 1. Loading packages, required functions, parameters, input data and initial state.
+## 2. Initial calculations and definition of variables
+## 3. Running a for loop that calculates fluxes and stocks over the simulation time
+## 4. Writing out model output
+
+# Required packages
+
+# Required functions
+
+# Load parameter file
+
+# Load input
+
+# Load initial state
+
+# Model run (for loop)
+
+# Write out output
+
+
